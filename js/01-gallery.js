c49a3c8b8eae9506c884613bc2e70539c842c442
BohdanDehodiuk <86683356+BohdanDehodiuk@users.noreply.github.com>
2021-11-12 21:32:08 +0200

    добавил json

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryConteiner = document.querySelector(".gallery");
+
+const galleryMarkup = createImagesGalleryMarkup(galleryItems);
+
+galleryConteiner.addEventListener("click", aoClickImageGallery);
+
+galleryConteiner.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createImagesGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>`;
+    })
+    .join("");
+}
+
+function aoClickImageGallery(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  console.log(e.target.dataset.source);
+}
+
+document.querySelector("button.image").onclick = () => {
+  basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`
+    )
+    .show();
+};
